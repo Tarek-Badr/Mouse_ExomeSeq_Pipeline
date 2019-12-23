92c3b1292db42b7a227391a5bc5e4b963caaf4b0
Tarek-Badr <43323534+Tarek-Badr@users.noreply.github.com>
2019-12-23 22:23:40 +0100

    Update bwamem_map.sh

--- 3- Alignment/bwamem_map.sh
+++ 3- Alignment/bwamem_map.sh
@@ -1,17 +1,16 @@
-#! /bin/sh
+#!/bin/bash
 
-sampleID=$1
-mkdir --parents scratchtarek${sampleID}
-mkdir --parents scratchtarek${sampleID}tmp
-tmpdir=scratchtarek${sampleID}tmp
-GATK= /home/tarek/Desktop/wxs/Tools/gatk-4.0.12.0/gatk 
-picard=java -Xmx30g -jar /home/tarek/Desktop/wxs/Tools/picard.jar
-bwaIndex=/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx
-mm10=/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx.fa
-bedfile=/home/tarek/Desktop/wxs/ex_region.sort.bed
-indelMills=/home/tarek/Desktop/wxs/Tools/mm10.indel.vcf
-dbsnp=/home/tarek/Desktop/wxs/Tools/mm10.snps.vcf
-bwamem=/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwa mem
+sampleID="AA-18-31-T"
+sample="/home/tarek/Desktop/wxs/Cleanseqs/${sampleID}"
+tmpdir="/home/tarek/Desktop/wxs/Tools/tmp"
+bwamem= "/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwa mem"
+bwaIndex="/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx"
+picard="java -Xmx30g -jar /home/tarek/Desktop/wxs/Tools/picard.jar"
+GATK="/home/tarek/Desktop/wxs/Tools/gatk-4.0.12.0/gatk"
+indelMills="/home/tarek/Desktop/wxs/Tools/mm10.indel.vcf"
+dbsnp="/home/tarek/Desktop/wxs/Tools/mm10.snps.vcf"
+mm10="/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx.fa"
+bedfile="/home/tarek/Desktop/wxs/ex_region.sort.bed"
 
-bwa mem -t 10 -M -R "@RG\tID:${sampleID}\tLB:NEXTERA\tPL:ILLUMINA\tSM:${sampleID}" \
-${bwaIndex} ${sample}_1.fq.gz ${sample}_2.fq.gz >${sample}.sam
+
+${bwamem} -t 10 -M -R "@RG\tID:${sampleID}\tLB:NEXTERA\tPL:ILLUMINA\tSM:${sampleID}" ${bwaIndex} ${sample}_1.fq.gz ${sample}_2.fq.gz > ${sampleID}.sam

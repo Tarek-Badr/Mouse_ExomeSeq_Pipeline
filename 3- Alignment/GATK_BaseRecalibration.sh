413c9a816cd4b8e7b3e28024e01ae944a09ba1b3
Tarek-Badr <43323534+Tarek-Badr@users.noreply.github.com>
2019-12-23 22:35:56 +0100

    Update GATK_BaseRecalibration.sh

--- 3- Alignment/GATK_BaseRecalibration.sh
+++ 3- Alignment/GATK_BaseRecalibration.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-sampleID=$1
-mkdir --parents scratchtarek${sampleID}
-mkdir --parents scratchtarek${sampleID}tmp
-tmpdir=scratchtarek${sampleID}tmp
-GATK= /home/tarek/Desktop/wxs/Tools/gatk-4.0.12.0/gatk 
-picard=java -Xmx30g -jar /home/tarek/Desktop/wxs/Tools/picard.jar
-bwaIndex=/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx
-mm10=/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx.fa
-bedfile=/home/tarek/Desktop/wxs/ex_region.sort.bed
-indelMills=/home/tarek/Desktop/wxs/Tools/mm10.indel.vcf
-dbsnp=/home/tarek/Desktop/wxs/Tools/mm10.snps.vcf
-bwamem=/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwa mem
+sampleID="AA-18-31-T"
+#sampleID=$1
+sample="/home/tarek/Desktop/wxs/Cleanseqs/${sampleID}"
+tmpdir="/home/tarek/Desktop/wxs/Tools/tmp"
+bwamem= "/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwa mem"
+bwaIndex="/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx"
+picard="java -Xmx30g -jar /home/tarek/Desktop/wxs/Tools/picard.jar"
+GATK="/home/tarek/Desktop/wxs/Tools/gatk-4.0.12.0/gatk"
+indelMills="/home/tarek/Desktop/wxs/Tools/mm10.indel.vcf"
+dbsnp="/home/tarek/Desktop/wxs/Tools/mm10.snps.vcf"
+mm10="/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx.fa"
+bedfile="/home/tarek/Desktop/wxs/ex_region.sort.bed"
 
 # indel realignment is no longer necessary for variant discovery using Mutec2 or other haplotype assembly steps in newer GATK
 #versions, therefore we only need the following steps before variant calling:  BaseRecalibrator, Apply Recalibration, AnalyzeCovariates (optional)
 
+${GATK} BaseRecalibrator \
+-R ${mm10} \
+-L ${bedfile}  \
+-I ${sampleID}.dedup.bam \
+--known-sites ${dbsnp} \
+--known-sites ${indelMills} \
+-O ${sampleID}.recal.data.table
 
-${GATK} BaseRecalibrator -R ${mm10} -L ${bedfile} -I {sample}.dedup.bam \
---known-sites ${dbsnp} --known-sites ${indelMills} -O {sample}.recal.data.table
+mv ${sampleID}.recal.data.table ${sampleID}.grp
 
-mv {sample}.recal.data.table {sample}.grp
+${GATK} ApplyBQSR \
+-bqsr ${sampleID}.grp \
+-I ${sampleID}.dedup.bam \
+-O ${sampleID}.recal.bam
 
-${GATK} ApplyBQSR -bqsr {sample}.grp -I {sample}.dedup.bam -O {sample}.recal.bam 
+#rm ${sampleID}.recal_data.grp ${sampleID}.realigned.bam ${sampleID}.realigned.bai
 
 #Now you have Analysis ready BAM files for variant calling by Mutec2 or Varscan or samtools.

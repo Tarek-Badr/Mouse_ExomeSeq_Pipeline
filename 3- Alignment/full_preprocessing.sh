4131926599a9df65f39b8315fa39d8fdd4f387f4
Tarek-Badr <43323534+Tarek-Badr@users.noreply.github.com>
2019-12-23 22:34:15 +0100

    Update full_preprocessing.sh

--- 3- Alignment/full_preprocessing.sh
+++ 3- Alignment/full_preprocessing.sh
@@ -12,19 +12,45 @@ dbsnp="/home/tarek/Desktop/wxs/Tools/mm10.snps.vcf"
 mm10="/home/tarek/Desktop/wxs/Tools/bwa-0.7.17/bwamm10index/mm10bwaidx.fa"
 bedfile="/home/tarek/Desktop/wxs/ex_region.sort.bed"
 
+${bwamem} \
+-t 10 \
+-M \
+-R "@RG\tID:${sampleID}\tLB:NEXTERA\tPL:ILLUMINA\tSM:${sampleID}" \ 
+${bwaIndex} \
+${sample}_1.fq.gz \ 
+${sample}_2.fq.gz > \
+${sampleID}.sam
 
-${bwamem} -t 10 -M -R "@RG\tID:${sampleID}\tLB:NEXTERA\tPL:ILLUMINA\tSM:${sampleID}" ${bwaIndex} ${sample}_1.fq.gz ${sample}_2.fq.gz > ${sampleID}.sam
-
-${picard} SortSam SO=coordinate INPUT=${sampleID}.sam OUTPUT=${sampleID}.bam VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true
+${picard} SortSam \
+SO=coordinate \
+INPUT=${sampleID}.sam \
+OUTPUT=${sampleID}.bam \
+VALIDATION_STRINGENCY=LENIENT \
+CREATE_INDEX=true
 
 #rm ${sampleID}.sam 
 
-${picard} MarkDuplicates INPUT=${sampleID}.bam OUTPUT=${sampleID}.dedup.bam VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true METRICS_FILE=${sampleID}.metrics.txt TMP_DIR=${tmpdir}
+${picard} MarkDuplicates \
+INPUT=${sampleID}.bam \
+OUTPUT=${sampleID}.dedup.bam \
+VALIDATION_STRINGENCY=LENIENT \
+CREATE_INDEX=true \
+METRICS_FILE=${sampleID}.metrics.txt \
+TMP_DIR=${tmpdir}
 
-${GATK} BaseRecalibrator -R ${mm10} -L ${bedfile}  -I ${sampleID}.dedup.bam --known-sites ${dbsnp} --known-sites ${indelMills} -O ${sampleID}.recal.data.table
+${GATK} BaseRecalibrator \
+-R ${mm10} \
+-L ${bedfile}  \
+-I ${sampleID}.dedup.bam \
+--known-sites ${dbsnp} \
+--known-sites ${indelMills} \
+-O ${sampleID}.recal.data.table
 
 mv ${sampleID}.recal.data.table ${sampleID}.grp
 
-${GATK} ApplyBQSR -bqsr ${sampleID}.grp -I ${sampleID}.dedup.bam -O ${sampleID}.recal.bam
+${GATK} ApplyBQSR \
+-bqsr ${sampleID}.grp \
+-I ${sampleID}.dedup.bam \
+-O ${sampleID}.recal.bam
 
 #rm ${sampleID}.recal_data.grp ${sampleID}.realigned.bam ${sampleID}.realigned.bai

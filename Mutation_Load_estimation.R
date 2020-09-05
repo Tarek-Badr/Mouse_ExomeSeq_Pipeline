82df175a2093a52f2f936be8491cc25d9a5eac99
Tarek-Badr <43323534+Tarek-Badr@users.noreply.github.com>
2020-09-05 15:55:26 +0200

    Add files via upload

---
+++ Mutation_Load_estimation.R
@@ -0,0 +1,7 @@
+install.packages("BiocManager")
+BiocManager::install("decompTumor2Sig")
+
+
+BiocManager::install("BSgenome.Mmusculus.UCSC.mm10")
+
+install.packages("g3viz", repos = "http://cran.us.r-project.org")
